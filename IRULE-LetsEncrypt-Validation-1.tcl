10947e4996377310e30d3730ba9681512cb8f3c1
Colin Stubbs <colin-stubbs@users.noreply.github.com>
2018-03-30 00:38:29 +1000

    Cleanup after improvements and testing

--- IRULE-LetsEncrypt-Validation-1.tcl
+++ IRULE-LetsEncrypt-Validation-1.tcl
@@ -1,9 +1,9 @@
 rule IRULE-LetsEncrypt-Validation-1 {
 when RULE_INIT {
-    set static::certbot_validator_class {certbot_validator}
+    set static::certbot_validator_class {letsencrypt_http-01}
 }
 when HTTP_REQUEST {
-    if { [string tolower [HTTP::uri]] starts_with {/.well-known/acme-challenge/} 
+    if { [string tolower [HTTP::uri]] starts_with {/.well-known/acme-challenge/}
       and [class match [HTTP::uri] ends_with ${static::certbot_validator_class}] != {} } {
         set response_content [class lookup [substr [HTTP::uri] 28] ${static::certbot_validator_class}]
         if { ${response_content} != {} } {
